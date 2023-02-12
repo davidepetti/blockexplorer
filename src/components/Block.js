dd3eb058bb26afac9dbba9034414e328ba3f7917
Davide Petti <petti.davide@gmail.com>
2023-02-12 20:10:53 +0100

    Add blocks section

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,9 +1,24 @@
+import { Box, Card, Flex, Spacer, Text } from '@chakra-ui/react';
+
 export default function Block({ block }) {
   return (
-    <div>
-      <p>{block.number}</p>
-      <p>{block.transactions.length}</p>
+    <Card>
+      <Flex>
+        <Box>
+          <Text>{block.number}</Text>
+          <Text color='blue'>
+            {Math.floor((Date.now() - block.timestamp) / 1000)}
+          </Text>
+        </Box>
+        <Spacer />
+        <Box>
+          <Text>
+            Validator {block.miner.slice(0, 8) + '...' + block.miner.slice(-8)}
+          </Text>
+          <Text>{block.transactions.length}</Text>
+        </Box>
+      </Flex>
       <hr />
-    </div>
+    </Card>
   );
 }
--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,14 +1,13 @@
+import { Box, Card, Heading } from '@chakra-ui/react';
 import Block from './Block';
 
 export default function Blocks({ blocks }) {
   return (
-    <div>
-      <h1>Blocks</h1>
-      <div>
-        {blocks.map((block) => (
-          <Block block={block} />
-        ))}
-      </div>
-    </div>
+    <Box w='50%' border='1px' borderColor='gray.200' borderRadius='lg'>
+      <Heading>Blocks</Heading>
+      {blocks.map((block) => (
+        <Block block={block} />
+      ))}
+    </Box>
   );
 }
