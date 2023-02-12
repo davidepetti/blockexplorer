ddb8c5124c81b1b324c329327685e762337b5d5f
Davide Petti <petti.davide@gmail.com>
2023-02-12 20:10:42 +0100

    Add main component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
+import { Box, Heading, Input } from '@chakra-ui/react';
+
 export default function Search() {
   return (
-    <div>
-      <form>
-        <input type='text' placeholder='Search by...'></input>
-      </form>
-    </div>
+    <Box my='10'>
+      <Heading size='md' my='5'>
+        The Ethereum Blockchain Explorer
+      </Heading>
+      <Input placeholder='Search by Address'></Input>
+    </Box>
   );
 }
